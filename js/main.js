a11a58fd346bc095b4459cfe0d45a4006ea7fa8e
ijemfornations <ijemfornations@gmail.com>
2025-12-24 15:39:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,3 +15,18 @@ fetch('data/testimonies.json')
       container.appendChild(div);
     });
   });
+function filterGallery(category) {
+  const items = document.querySelectorAll('.gallery-item');
+
+  items.forEach(item => {
+    if (category === 'all') {
+      item.style.display = 'block';
+    } else {
+      if (item.classList.contains(category)) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    }
+  });
+}
